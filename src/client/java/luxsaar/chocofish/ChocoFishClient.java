d3be2ae995736da9bddfc6ecd867d3d1cc70c2ca
Lucas Eusebio <lucaseusebio@Lucass-MacBook-Air.local>
2023-06-28 02:37:34 -0400

    Initial commit

---
+++ src/client/java/luxsaar/chocofish/ChocoFishClient.java
@@ -0,0 +1,10 @@
+package luxsaar.chocofish;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChocoFishClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
